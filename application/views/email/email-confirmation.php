936b1a736d1c128a0e6b2641ed836824043ea65f
Catalin Olteanu <cata_ocg@yahoo.com>
2016-12-09 22:57:16 +0200

    emails

---
+++ application/views/email/email-confirmation.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Gravitas+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ramaraja" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+</head>
+<body style="background: #C7EBFC; color: #2B3990; width: 100%; height: 100%; }">
+<div>
+    <div class="container" style="max-width: 800px; margin: 0; padding: 30px 0;">
+        <div class="text" style="font-weight: bold; text-align: left; margin: 100px 50px 150px; font-size: 20.63px; font-family: \"Ramaraja\",regular;">
+        <div style="margin: 30px 0 30px 0;">
+            Mosule, esti aproape Mos Craciun! </br>
+            Da click <a href="{unwrap}<?php echo $link; ?>{/unwrap}">aici</a> pentru a activa contul. </br>
+        </div>
+        <div style="margin: 30px 0 30px 0;">
+            Atentie! Dupa activarea contului, vei fi directionat in santachat.ro, care de acum inainte va ramane pagina din care copilul ii va scrie Mosului. </br>
+            Tu nu uita sa verifici casuta de mail, inclusiv in spam, ca sa citesti scrisorile copilului si sa-i raspunzi.
+        </div>
+        <div style="margin: 30px 0 30px 0;">Iar daca link-ul de mai sus nu merge, atunci copiaza aceasta adresa in browser:</div>
+        <div class="url" style="margin: 30px 0 30px 0;"><a href="{unwrap}<?php echo $link; ?>{/unwrap}">{unwrap}<?php echo $link; ?>{/unwrap}</a></div>
+        <div>Iar daca vrei sa ne cunosti mai bine si sa afli cum iti putem proteja copilul de problemele mai pamantesti ale acestui sezon, cum ar fi raceala, gripa, tusea, raguseala sau febra, te asteptam pe <a href="http://minunino.ro">minunino.ro</a> sau pe <a href="facebook.com/minunino">facebook</a>.</div>
+        <div class="image" style=" margin: 75px 0 30px 0;">
+            <img src="http://santachat.ro/public/images/email.png" alt="M" height="49" width="409">
+        </div>
+    </div>
+</div>
+</div>
+</body>
+</html>
